41046a24f2ba71935eb7a229d5b403441c21f16f
unknown <mex3woof@gmail.com>
2021-10-25 16:46:46 +0800

    Test heroku

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ origins = [
     'http://localhost:3000'
 ]
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials = True,
     allow_methods=['GET'],
-    allow_headers=['Content-Type', 'application/xml'],
+    allow_headers=['*'],
 )
 
 @app.get('/')
